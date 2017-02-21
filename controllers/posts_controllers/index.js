d4586c812c4067c4de332f0e17b618cb0fd49988
Joe Kitaj <joekitaj@gmail.com>
2017-02-21 12:09:32 -0500

    Updated routes

--- controllers/posts_controllers/index.js
+++ controllers/posts_controllers/index.js
@@ -10,10 +10,10 @@ router.get('/', controller.login);
 router.post('/login', controller.saveUser);
 
 router.get('/posts', controller.index);
-router.get('/new', controller.newPost);
-router.get('/:id', controller.show);
+router.get('/posts/new', controller.newPost);
+router.get('/posts/:id', controller.show);
 
-router.post('/', controller.addPost);
+router.post('/posts/new', controller.addPost);
 router.post('/:id', controller.addComment);
 
 
