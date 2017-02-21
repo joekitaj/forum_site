13b5c8fdbf0604dff4454a1ecb404ce7298a80a4
Joe Kitaj <joekitaj@gmail.com>
2017-02-21 13:53:06 -0500

    Set up project for Heroku Deployment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const connection = 'postgress://localhost:5432/forum';
+const connection = process.env.DATABASE_URL || 'postgress://localhost:5432/forum';
 
 const database = pgp(connection);
 
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
