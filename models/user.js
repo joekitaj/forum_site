f254b99f0f7077ddf8e3bbf742b9cda44f2e8480
Joe Kitaj <joekitaj@gmail.com>
2017-02-23 20:33:20 -0500

    Added find by username functionality

--- models/user.js
+++ models/user.js
@@ -2,6 +2,13 @@ const db = require('../config/db');
 const bcrypt = require('bcrypt')
 const User = {};
 
+User.findByUsername = (username) => {
+  return db.oneOrNone(`
+    SELECT * FROM users
+    WHERE username = $1`,
+    [username])
+}
+
 User.save = (user) => {
   user.password = bcrypt.hashSync(user.password, 10);
 
