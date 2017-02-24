deeeb156ec50bc51a5cacc17f33a030053b837c4
Joe Kitaj <joekitaj@gmail.com>
2017-02-24 16:27:17 -0500

    Added new route

--- resources.js
+++ resources.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.redirect('/posts')
-})
-
 router.use('/posts', require('./controllers/posts_controllers'));
 
 router.use('/users', require('./controllers/users_controllers'));
+
+router.get('/', (req, res) => {
+  res.redirect('/posts')
+});
+
 
 module.exports = router;
