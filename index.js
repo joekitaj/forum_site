c8d6006aa16ed162c653886a91516db6f2bb18b0
Joe Kitaj <joekitaj@gmail.com>
2017-02-24 11:13:12 -0500

    Moved initial redirect to router

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.redirect('/posts')
-})
-
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
--- resources.js
+++ resources.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.redirect('/posts')
+})
+
 router.use('/posts', require('./controllers/posts_controllers'));
 
 router.use('/users', require('./controllers/users_controllers'));
